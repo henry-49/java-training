a67ca8e6c53f6237e6794a03a3a1c0f4f6a51bfa
henry-49 <36896215+henry-49@users.noreply.github.com>
2018-10-29 17:43:58 -0700

    Update NumberCal.java

--- src/basics/NumberCal.java
+++ src/basics/NumberCal.java
@@ -5,6 +5,7 @@ public class NumberCal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Number Cal");
+		System.out.println("Adding code from Github");
 	}
 
 }
